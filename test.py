a2817c00cd398cb9340fd60a730d943a4a715645
UltralyticsAssistant <web@ultralytics.com>
2026-07-06 04:13:02 +0000

    Auto-format by https://ultralytics.com/actions

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import torch
 # 1. First, define your model class (must be the same architecture as the saved one)
 
 # 2. Load the state_dict
-checkpoint = torch.load('/home/bongmedai/Endo/ultralytics/runs/segment/train5/weights/best.pt', weights_only=False)
+checkpoint = torch.load("/home/bongmedai/Endo/ultralytics/runs/segment/train5/weights/best.pt", weights_only=False)
 print("Checkpoint keys:", checkpoint.keys())
 print(checkpoint["epoch"])
 print(checkpoint["train_args"])
