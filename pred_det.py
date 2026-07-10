198482f90616e50e1fb75f204d16308064ea8c23
UltralyticsAssistant <web@ultralytics.com>
2026-07-10 06:59:08 +0000

    Auto-format by https://ultralytics.com/actions

--- pred_det.py
+++ pred_det.py
@@ -2,7 +2,9 @@ from ultralytics import YOLO
 
 model = YOLO("/home/bongmedai/Endo/ultralytics/runs/detect/yolo_l_ade_carci/weights/best.pt")
 
-results = model.val(data="/home/bongmedai/Endo/datasets/medai_endo_det/medai_endo_det.yaml", plots=True, save=True, save_txt=True)
+results = model.val(
+    data="/home/bongmedai/Endo/datasets/medai_endo_det/medai_endo_det.yaml", plots=True, save=True, save_txt=True
+)
 # results = model.predict(
 #     source="/home/bongmedai/Endo/datasets/endo_coco_crop/images/val",
 #     save=True,
